52b48805a74c7337a794307a0cc1ecfe3a1c66ad
alfiannnas <alfian.baits@gmail.com>
2023-12-14 13:17:05 +0700

    feat(button): practice useState on Button part 2

--- src/Components/Second.tsx
+++ src/Components/Second.tsx
@@ -2,11 +2,7 @@ import React, { useState } from "react";
 import img from "../assets/test.png";
 
 export default function Second(props) {
-  const [getNameValue, SetNameValue] = useState("");
-
-  const changeName = () => {
-    SetNameValue("Nindy");
-  };
+  const [getPacarSaya, setPacarSaya] = useState(0);
 
   return (
     <>
@@ -15,12 +11,12 @@ export default function Second(props) {
           <div className="container max-w-6xl absolute top-24 flex flex-col items-center md:relative md:top-0 md:relative md:top-0 md:flex-row md:items-center">
             <img src={img} className="rounded-full w-52 md:w-80 sm:w-64 border-4 border-sky-950 mb-4 md:mb-0 mr-5" />
             <div>
-              <p>{getNameValue}</p>
+              <h1>{getPacarSaya} saya</h1>
               <h1 className="text-xl font-poppins md:text-5xl sm:text-3xl m-4">{props.title}</h1>
               <p className="text-white text-md sm:text-xl md:text-xl m-4">{props.text}</p>
             </div>
           </div>
-          <button className="bg-sky-800" onClick={() => changeName()}>
+          <button className="bg-sky-800" onClick={() => setPacarSaya((prev) => prev + 1)}>
             ok
           </button>
         </div>
