8c6a319d6c679cc9311d2d202044af504bda3a89
alfiannnas <alfian.baits@gmail.com>
2023-12-14 14:52:33 +0700

    feat(button): practice useEffect on Button

--- src/Components/Second.tsx
+++ src/Components/Second.tsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import img from "../assets/test.png";
 
 export default function Second(props) {
   const [getPacarSaya, setPacarSaya] = useState(0);
+  const [getNamaPacar, setNamaPacar] = useState("");
+
+  useEffect(() => {
+    if (getPacarSaya > 1) {
+      setNamaPacar("Nindy");
+    }
+  }, [getPacarSaya]);
 
   return (
     <>
@@ -11,6 +18,7 @@ export default function Second(props) {
           <div className="container max-w-6xl absolute top-24 flex flex-col items-center md:relative md:top-0 md:relative md:top-0 md:flex-row md:items-center">
             <img src={img} className="rounded-full w-52 md:w-80 sm:w-64 border-4 border-sky-950 mb-4 md:mb-0 mr-5" />
             <div>
+              <h1> {getNamaPacar}</h1>
               <h1>{getPacarSaya} saya</h1>
               <h1 className="text-xl font-poppins md:text-5xl sm:text-3xl m-4">{props.title}</h1>
               <p className="text-white text-md sm:text-xl md:text-xl m-4">{props.text}</p>
